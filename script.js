3d8ccd5a03b6d35a13d18d606fb44d40aabd7238
kumkum145 <riyachawla939@gmail.com>
2026-03-15 22:13:49 -0600

    Added assignment tracker project

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value.trim();
+
+    if (task === "") {
+        alert("Please enter an assignment");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task + " ";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
